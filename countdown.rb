d5af40cf7fb0b1a5582c03ccf7e455032c71d66b
Larissa S <larissafschiavo@gmail.com>
2017-11-01 03:35:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(n)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(5)
+  x = n
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end 
